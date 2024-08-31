02d8a1b55099fbca17f224b72bc972728116324a
Yura3301 <win_w@bk.ru>
2024-08-31 11:39:35 +0300

    Delete module_2_2.py

--- module_2_2.py
+++
@@ -1,9 +0,0 @@
-first = input(print('Первое число: '))
-second = input(print('Второе число: '))
-third  = input(print('Третье число: '))
-if first == second and second == third:
-    print(3)
-elif (first == second or second == third or first == third):
-    print(2)
-else:
-    print(0)
