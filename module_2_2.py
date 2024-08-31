0e4f63edb5ac4948a2a855058d7a25f3f72b3e52
Yura3301 <win_w@bk.ru>
2024-08-31 11:36:46 +0300

    Delete module_2_2.py

--- module_2_2.py
+++
@@ -1,9 +0,0 @@
-first = 1
-second = 2
-third  = 3
-if first == second and second == third:
-    print(3)
-elif (first == second or second == third or first == third):
-    print(2)
-else:
-    print(0)
